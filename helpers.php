c461aa587703222153e86a14fc68c76e4936ff82
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2024-04-25 03:40:19 -0700

    added inspect and die function for debugging

--- helpers.php
+++ helpers.php
@@ -22,6 +22,8 @@ function loadView($name) {
 
    $viewPath = basePath("views/{$name}.view.php");
 
+   inspectAndDie($name);
+
    if (file_exists($viewPath)) {
        require $viewPath;
    } else {
@@ -36,11 +38,32 @@ function loadView($name) {
  */
 function loadPartial($name) {
     $partialsPath = basePath("views/partials/{$name}.php");
-    
+
     if (file_exists($partialsPath)) {
         require $partialsPath;
     }
     else {
          echo "Partials '{$name} not found!'";
     }
+}
+
+/**
+ *   INSPECT A VALUE(s)
+ * 
+ *    @param mixed $value
+ *    @return void
+ * 
+ */
+
+ function inspect($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+ }
+
+function inspectAndDie($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
 }
