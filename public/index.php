46b5cd787d70a9c134d85f51c410e56774c72eed
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2024-06-01 21:43:35 -0700

    added dabatabase class and controllers

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
     require "../helpers.php";
     require basePath("Router.php");
+    require basePath("Database.php");
 
+    // Instatiating the router
     $router = new Router();    
+
+    //Get routes
     $routes = require basePath("routes.php");
     
+    // GET current URI AND HTTP METHOD
     $uri = $_SERVER["REQUEST_URI"];
     $method = $_SERVER["REQUEST_METHOD"];
   
+    //ROUTE THE REQUEST
     $router->route($uri, $method);
 ?>
